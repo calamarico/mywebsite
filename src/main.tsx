026f2358a2ac51136262c517d5215090c6cc1349
Daniel Hernández <daniel.hernandez.zafra@gmail.com>
2026-05-01 02:15:24 +0200

    Add logs

--- src/main.tsx
+++ src/main.tsx
@@ -17,6 +17,8 @@ console.log(
   'color: #aaa; font-size: 14px; font-weight: normal;',
   'color: #e0a0ff; font-size: 10px; font-family: monospace; font-weight: bold;'
 )
+console.log("%c💻 Kalamarico", "font-size:20px; color:#a3e635;");
+console.log("Senior Frontend Engineer @ Kalamarico\n\n");
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
